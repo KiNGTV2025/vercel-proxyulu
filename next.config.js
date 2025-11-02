4b8205aa468f0b95d884ac4e76c46f039b4737e6
KiNGTV2025 <goldtowerumit@gmail.com>
2025-11-03 01:55:48 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,POST' },
+          { key: 'Access-Control-Allow-Headers', value: '*' },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
